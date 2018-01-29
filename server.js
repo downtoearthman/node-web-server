e96847b0da113b1ee6003d8e9889187a879d1394
Paul Saenz <imperativeemarketing@gmail.com>
2018-01-28 22:53:29 -0800

    After completing Lecture 44

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
       pageTitle: 'Home Page',
       welcomeMessage: 'Welcome to our website',
-      currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 //  res.send('About Page');
 });
